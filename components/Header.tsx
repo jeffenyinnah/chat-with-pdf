0fe638bf6fc58845a645d05faa0b73ac2a19931e
jeffenyinnah <jeffenyinnah@gmail.com>
2024-07-27 04:01:57 +0200

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { File, FilePlus } from "lucide-react";
+
+function Header() {
+  return (
+    <div className="flex justify-between bg-white shadow-sm p-5 border-b">
+      <Link href="/dashboard" className="text-2xl">
+        Chat to <span className="text-indigo-600">PDF</span>
+      </Link>
+
+      <SignedIn>
+        <div className="flex items-center space-x-2">
+          <Button asChild variant="link" className="hidden md:flex">
+            <Link href="/dashboard/upgrade">Pricing</Link>
+          </Button>
+
+          <Button asChild variant="outline">
+            <Link href="/dashboard" className="p-1">
+              My Documents
+            </Link>
+          </Button>
+
+          <Button asChild variant="outline" className="border-indigo-600">
+            <Link href="/dashboard/upload">
+              <FilePlus className="text-indigo-600 animate-bounce" />
+            </Link>
+          </Button>
+          <UserButton />
+        </div>
+      </SignedIn>
+    </div>
+  );
+}
+
+export default Header;
